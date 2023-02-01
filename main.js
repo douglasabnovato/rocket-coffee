ccd89cdb45d012943097721a5503632a6e9b696d
douglasabnovato <douglasabnovato@gmail.com>
2023-02-01 15:21:56 -0300

    estilizando os elementos

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Starting ...")
+
+let show = true;
+
+const menuSection = document.querySelector(".menu-section");
+const menuToggle = menuSection.querySelector(".menu-toggle");
+
+menuToggle.addEventListener("click", () => {
+  document.body.style.overflow = show ? "hidden" : "initial";
+  menuSection.classList.toggle("on", show);
+  show = !show;
+});
